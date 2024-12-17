d129c1f869613ee603ab3d5d5fa3631e222daa80
jamesrota <jrota@ualberta.ca>
2024-12-16 20:16:57 -0700

    Added some create, update functions to the database

--- Backend-Development/functions.py
+++ Backend-Development/functions.py
@@ -7,7 +7,79 @@
     Last Modified: Dec 16, 2024
 
 """
+import sqlite3
 
 # Step Two
 # Implement endpoints to create, read, update, and delete for all resources.
+
+# Create the connection
+connection = sqlite3.connect("REST.db")
+
+# Allow foreign keys
+connection.execute("PRAGMA foreign_keys = ON")
+
+# Create cursor
+cursor = connection.cursor()
+
+# Create Statements
+
+def Create_new_clinician(NPI_NUM, name, specialty):
+    cursor.execute("INSERT OR IGNORE INTO clinician VALUES(?, ?, ?)", (NPI_NUM, name, specialty)) # added the IGNORE TO AVOID DUPLICATES
+
+def Create_new_patient(patient_id, name, clinician_id, specialty):
+    cursor.execute("INSERT INTO patient VALUES(?, ?, ?, ?)", (patient_id, name, clinician_id, specialty))
+
+def Create_new_appointment(app_id, clinician_id, patient_id,  appointment_time):
+    cursor.execute("INSERT INTO appointment VALUES(?, ?, ?, ?)", (app_id, clinician_id, patient_id,  appointment_time))
+
+# Read Statements
+
+def get_appointments_info():
+    appointment_info = cursor.execute("""
+                   SELECT appointment.appointment_time, clinician.name AS clinician_name, patient.name AS patient_name
+                   FROM appointment
+                   JOIN clinician ON appointment.clinician_id = clinician.NPI_NUM
+                   JOIN patient ON appointment.patient_id = patient.id
+                   """).fetchall()
+    
+    print(f'appointments_info: {appointment_info}')
+
+# Update Statements
+
+# Helper function to be used in updating patient's clinician, have to make sure the clinician exists
+def check_if_clinician_exists(clinician_id):
+
+    cursor.execute("SELECT COUNT(*) FROM clinician WHERE NPI_NUM = ?", (clinician_id,))
+    
+    # Fetch the result
+    count = cursor.fetchone()[0]
+    
+    # Return True if the ID exists, otherwise False
+    
+    if count > 0:
+        return True
+    
+    return False
+
+def update_patients_clinician(clinician_id, patient_id):
+
+    query = """
+            UPDATE patient
+            SET clinician_id = ?
+            WHERE id = ?;
+
+            """
+    
+    if check_if_clinician_exists(clinician_id) == True:
+        cursor.execute(query, (clinician_id, patient_id))
+    else:
+        print("Sorry, the process cannot be done, clinician does not exist")
+
+
+# TEST 
+
+
+
+
+connection.close()
 
--- Backend-Development/index.py
+++ Backend-Development/index.py
@@ -10,7 +10,7 @@ Requirements:
 
 3. Use an SQLlite database to store the data.
 
-4. Validate incoming request data.
+4. Validate incoming request data - DO NOT KNOW HOW TO DO THIS.
 
 5. Be sure to include an NPI number in the clinician table.
    --> An NPI number is a unique identification number for covered health care
@@ -51,47 +51,47 @@ cursor = connection.cursor()
 #Create the tables
 
 # Clinician table - (NPI (Primary Key), clinician name, specialty (Anixety, Depression, etc))
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS clinician (
-    NPI_NUM INTEGER PRIMARY KEY, 
-    name TEXT, 
-    specialty TEXT
-)
-""")
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS clinician (
+#     NPI_NUM INTEGER PRIMARY KEY, 
+#     name TEXT, 
+#     specialty TEXT
+# )
+# """)
 
-# Patient table - (id (Primary Key), patient name, 
-#                  clinician_id (forign key from clinician table),
-#                  trouble (Why the patient is seeking therapy))
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS patient (
-    id INTEGER PRIMARY KEY,
-    name TEXT, 
-    clinician_id INTEGER, 
-    trouble TEXT,
-    FOREIGN KEY (clinician_id) REFERENCES clinician(NPI_NUM) ON DELETE CASCADE
-)
-""")
+# # Patient table - (id (Primary Key), patient name, 
+# #                  clinician_id (forign key from clinician table),
+# #                  trouble (Why the patient is seeking therapy))
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS patient (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT, 
+#     clinician_id INTEGER, 
+#     trouble TEXT,
+#     FOREIGN KEY (clinician_id) REFERENCES clinician(NPI_NUM) ON DELETE CASCADE
+# )
+# """)
 
-# Appointment table - (id (Primary Key), clinician_id (foreign key),
-#                      patient_id (foreign key), 
-#                      appointment_time (Time the appointment takes place) )
-#
+# # Appointment table - (id (Primary Key), clinician_id (foreign key),
+# #                      patient_id (foreign key), 
+# #                      appointment_time (Time the appointment takes place) )
+# #
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS appointment (
-    id INTEGER PRIMARY KEY,
-    clinician_id INTEGER, 
-    patient_id INTEGER, 
-    appointment_time TEXT,
-    FOREIGN KEY (clinician_id) REFERENCES clinician(NPI_NUM) ON DELETE CASCADE,
-    FOREIGN KEY (patient_id) REFERENCES patient(id) ON DELETE CASCADE
-)
-""")
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS appointment (
+#     id INTEGER PRIMARY KEY,
+#     clinician_id INTEGER, 
+#     patient_id INTEGER, 
+#     appointment_time TEXT,
+#     FOREIGN KEY (clinician_id) REFERENCES clinician(NPI_NUM) ON DELETE CASCADE,
+#     FOREIGN KEY (patient_id) REFERENCES patient(id) ON DELETE CASCADE
+# )
+# """)
 
 #Inserting sample data
 #cursor.execute("INSERT INTO clinician VALUES(?, ?, ?)", (1, "John", "Anxiety"))
 #cursor.execute("INSERT INTO patient VALUES(?, ?, ?, ?)", (1, "Karen", 1, "Depression"))
 #cursor.execute("INSERT INTO appointment VALUES(?, ?, ?, ?)", (1, 1, 1, "12:30"))
 
-connection.commit()
-connection.close()
+connection.commit() # commit changes
+connection.close() # close the connection
